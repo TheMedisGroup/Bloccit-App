53c7ec59baa5a9af336c7f8d1df699372c5329df
Michael Medis <TheMedisGroup@gmail.com>
2015-10-19 14:11:34 -0400

    Assignment 26 completed

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
+
   describe "GET index" do
     it "renders the index template" do
       get :index
@@ -8,10 +9,21 @@ RSpec.describe WelcomeController, type: :controller do
       expect(response).to render_template("index")
     end
   end
+
   describe "GET about" do
     it "renders the about template" do
       get :about
+
       expect(response).to render_template("about")
     end
   end
+
+  describe "GET faq" do
+    it "renders the faq template" do
+      get :faq
+
+      expect(response).to render_template("faq")
+    end
+  end
+
 end
