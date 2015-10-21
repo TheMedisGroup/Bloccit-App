a1b4e315a33b6fbfdc1573efbc75a058e0148bbf
Michael Medis <TheMedisGroup@gmail.com>
2015-10-21 16:30:53 -0400

    Assignment 30 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,8 @@ posts = Post.all
   )
 end
 
+Post.find_or_create_by(title: "I have miles to go and promises to keep before I get to sleep before I get to sleep.", body:"What you read when you don't have to determines what you will be when you can't help it.")
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
