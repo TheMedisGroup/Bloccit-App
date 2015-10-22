e72a33c73ee72f6f0e04d489f8c22b4154dd45ae
Michael Medis <TheMedisGroup@gmail.com>
2015-10-22 10:09:49 -0400

    remove spam assigment from assignment 31 cause in checkpoint 32

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,6 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-    # puts @posts.count
-    @posts.each_with_index do |post, index|
-      if index % 5 == 0
-        post.title = "SPAM"
-      end
-    end
   end
 
   def show
