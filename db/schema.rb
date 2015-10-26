178c4ddd130444f231436ddd409b8449dcc4151f
Michael Medis <TheMedisGroup@gmail.com>
2015-10-26 17:36:06 -0400

    Checkpoint 36, completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024192807) do
+ActiveRecord::Schema.define(version: 20151026201623) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -20,15 +20,6 @@ ActiveRecord::Schema.define(version: 20151024192807) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "answers", force: :cascade do |t|
-    t.integer  "question_id"
-    t.text     "body"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -57,12 +48,31 @@ ActiveRecord::Schema.define(version: 20151024192807) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sponsored_posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "price"
+    t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "sponsored_posts", ["topic_id"], name: "index_sponsored_posts_on_topic_id"
+
   create_table "topics", force: :cascade do |t|
     t.string   "name"
     t.boolean  "public",      default: true
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
