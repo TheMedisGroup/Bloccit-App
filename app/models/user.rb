4713a45f070da3d8ada33f13031361f1c7e9ab7f
Michael Medis <TheMedisGroup@gmail.com>
2015-10-30 10:44:45 -0400

    Checkpoint 40 completed to Seed Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :posts
 
   before_save { self.email = email.downcase }
-  before_save { self.name.split(" ")[0] }
+  before_save { self.role ||= :member }
 
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -18,4 +18,6 @@ class User < ActiveRecord::Base
              format: { with: EMAIL_REGEX }
 
    has_secure_password
+
+   enum role: [:member, :admin]
 end
