4bfb2b17e2ba65e8b3bb444120d02699b8ba9d9f
Michael Medis <TheMedisGroup@gmail.com>
2015-11-02 16:47:50 -0500

    Checkpoint 42 Completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,42 +15,44 @@ class PostsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.build(post_params)
     @post.user = current_user
-     if @post.save
-       flash[:notice] = "Post was saved."
-       redirect_to [@topic, @post]
-     else
-       flash[:error] = "There was an error sving the post. Please try again."
-       render :new
-     end
-   end
 
-   def edit
+    if @post.save
+      @post.labels = Label.update_labels(params[:post][:labels])
+      flash[:notice] = "Post was saved."
+      redirect_to [@topic, @post]
+    else
+     flash[:error] = "There was an error sving the post. Please try again."
+     render :new
+    end
+  end
+
+  def edit
     @post = Post.find(params[:id])
-   end
+  end
 
-   def update
+  def update
     @post = Post.find(params[:id])
     @post.assign_attributes(post_params)
-     if @post.save
-       flash[:notice] = "Post was updated."
-       redirect_to [@post.topic, @post]
-     else
-       flash[:error] = "There was an error saving the post. Please try again."
-       render :edit
-     end
+    if @post.save
+      flash[:notice] = "Post was updated."
+      redirect_to [@post.topic, @post]
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :edit
+    end
    end
 
-   def destroy
-      @post = Post.find(params[:id])
+  def destroy
+    @post = Post.find(params[:id])
 
-      if @post.destroy
-        flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-        redirect_to @post.topic
-      else
-        flash[:error] = "There was an error deleting the post."
-        render :show
-      end
+    if @post.destroy
+      flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+      redirect_to @post.topic
+    else
+      flash[:error] = "There was an error deleting the post."
+      render :show
     end
+  end
 
   private
   def post_params
