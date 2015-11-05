807369e54ae766f38e0a263995b837461ef56d11
Michael Medis <TheMedisGroup@gmail.com>
2015-11-05 10:50:41 -0500

    fixes to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,4 @@
-class PostController < ApplicationController
+class PostsController < ApplicationController
   before_action :require_sign_in, except: :show
   before_action :authorize_user, except: [:show, :new, :create]
 
